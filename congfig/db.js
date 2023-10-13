fe3c61350f4725891003a03b3ae3994a3e6e4678
Niks-World <nikkalyan52@gmail.com>
2023-10-14 01:13:19 +0530

    backend depandec

---
+++ congfig/db.js
@@ -0,0 +1,12 @@
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+
+// const connection= async()=>{
+//   await mongoose.connect(process.env.mongoDBUrl);
+
+//   console.log("connection is done");
+
+// }
+// module.exports = {
+//     connection 
+// }
